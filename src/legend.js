c1dd1379a972184e9b9a8ff7180ac21734313a0b
Bob Haslett <bob.haslett@ft.com>
2017-08-16 18:09:15 +0100

    Add script ids to legen

--- src/legend.js
+++ src/legend.js
@@ -84,6 +84,9 @@ export default function drawLegend() {
                 .on("start", dragstarted)
                 .on("drag", dragged)
                 .on("end", dragended));
+
+        parent.selectAll(".chart-subtitle")
+            .attr('id',frameName + 'legend')
       }
 
 
