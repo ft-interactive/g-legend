6699ca9618ff6f92bb7d6bebe7750208c8132a3a
Ændrew Rininsland <andrew.rininsland@ft.com>
2017-09-14 13:33:03 +0100

    Linting after merging #6.

--- test/g-legend-test.js
+++ test/g-legend-test.js
@@ -1,14 +1,14 @@
-var tape = require('tape');
+const tape = require('tape');
 const glegend = require('../').legend;
 const d3 = require('d3');
 
-tape('rem should equal 10.', function(test) {
+tape('rem should equal 10.', (test) => {
     const legend = glegend();
     test.equal(legend.rem(), 10);
     test.end();
 });
 
-tape('should accept arbitrary colour scales', function(test) {
+tape('should accept arbitrary colour scales', (test) => {
     const colorScale = d3.scaleOrdinal();
     const legend = glegend().colourPalette(colorScale);
     test.equal(colorScale, legend.colourPalette());
